cb6ba13a0c5c7aa00da3b76085e30fb00cbc85b2
linnypham <89488749+linnypham@users.noreply.github.com>
2024-09-03 13:18:22 -0400

    binary

---
+++ binary.py
@@ -0,0 +1,23 @@
+def binary(number):
+    bin = []
+    function = []
+
+    while number > 0:
+        remainder = number % 2
+        number //= 2
+        bin.append(remainder)
+        
+    for i in range(len(bin)):
+        if bin[i]==1:
+            function.append(f'2^{i}')
+
+    function.reverse()
+    output1 = '+'.join(map(str, function))
+    print(f'Binary formula: {output1}')
+
+    bin.reverse()
+    output2 = ''.join(map(str, bin))
+    print(f'Binary number: {output2}')
+
+a = int(input('Input integer: '))
+print(binary(a))
