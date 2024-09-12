3e29f2c22ee16dfdac5a395e40f21f94cc61cf34
linnypham <89488749+linnypham@users.noreply.github.com>
2024-09-12 12:11:30 -0400

    binrayr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from binary import binaryToInteger
-a = binaryToInteger(1101)
-b = binaryToInteger(1111)
-print(a)
-print(b)
-print(a + b)
+from converter import toBinary,toHex
+
+print(toBinary(16))
+print(toHex(16))
